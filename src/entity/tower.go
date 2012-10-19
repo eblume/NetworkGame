1956faf6f16f30758ad39293c6a23c999e2d0710
Erich Blume <blume.erich@gmail.com>
2012-10-18 20:15:44 -0700

    OO cleanup

--- src/entity/tower.go
+++ src/entity/tower.go
@@ -1,58 +1,70 @@
 package entity
 
 type Tower struct {
-	connect_to        chan *Tower
-	disconnect_from   chan *Tower
-	destruct          chan bool
-	get_num_neighbors chan int
-	neighbors         map[*Tower]bool
+    connect_to        chan *Tower
+    disconnect_from   chan *Tower
+    destruct          chan bool
+    get_num_neighbors chan chan int
+    neighbors         map[*Tower]bool
 }
 
 func NewTower() *Tower {
-	return &Tower{
-		connect_to:        make(chan *Tower),
-		disconnect_from:   make(chan *Tower),
-		destruct:          make(chan bool),
-		get_num_neighbors: make(chan int),
-		neighbors:         make(map[*Tower]bool),
-	}
+    t := &Tower{
+        connect_to:        make(chan *Tower),
+        disconnect_from:   make(chan *Tower),
+        destruct:          make(chan bool),
+        get_num_neighbors: make(chan chan int),
+        neighbors:         make(map[*Tower]bool),
+    }
+    go runTower(t)
+    return t
 }
 
-// Goroutine to run the specified tower
-func RunTower(t *Tower) {
-	for {
-
-		num_neighbors := len(t.neighbors)
-
-		select {
-		case t.destruct <- true:
-			for neighbor := range t.neighbors {
-				neighbor.disconnect_from <- t
-			}
-			return
-
-		case other := <-t.connect_to:
-			t.neighbors[other] = true
-
-		case other := <-t.disconnect_from:
-			delete(t.neighbors, other)
-
-		case t.get_num_neighbors <- num_neighbors:
-			// pass
-
-		}
-	}
+func (t *Tower) GetNumNeighbors() int {
+    recv := make(chan int)
+    t.get_num_neighbors <- recv
+    return <- recv
 }
 
-// Join the two specified towers.
-func JoinTowers(a *Tower, b *Tower) {
-	a.connect_to <- b
-	b.connect_to <- a
+func (t *Tower) JoinTower(other *Tower) {
+    t.connect_to <- other
+    other.connect_to <- t
 }
 
-// Seperate the two specified towers.
-
-func DisjoinTowers(a *Tower, b *Tower) {
-	a.disconnect_from <- b
-	b.disconnect_from <- a
+func (t *Tower) DisjoinTower(other *Tower) {
+    t.disconnect_from <- other
+    other.disconnect_from <- t
 }
+
+func (t *Tower) Stop() {
+    <- t.destruct
+}
+
+
+// INTERNALS
+
+func runTower(t *Tower) {
+    for {
+        select {
+        case recv := <- t.get_num_neighbors:
+            recv <- len(t.neighbors)            
+
+        case other := <- t.connect_to:
+            t.neighbors[other] = true
+
+        case other := <- t.disconnect_from:
+            delete(t.neighbors, other)
+
+        case t.destruct <- true:
+            destruct(t)
+            return
+        }
+    }
+}
+
+func destruct(t *Tower) {
+    for other := range t.neighbors {
+        other.disconnect_from <- t
+    }
+}
+
--- src/entity/tower_test.go
+++ src/entity/tower_test.go
@@ -12,42 +12,42 @@ func TestTowerConnections(t *testing.T) {
 	c := NewTower()
 	towers := []*Tower{a, b, c}
 
-	// Start up the towers
-	go RunTower(a)
-	go RunTower(b)
-	go RunTower(c)
-
 	// Verify none have any neighbors:
 	for tower := range towers {
-		if num := <-towers[tower].get_num_neighbors; num != 0 {
+		if num := towers[tower].GetNumNeighbors(); num != 0 {
 			t.Error("Tower had neighbors before adding any.")
 		}
 	}
 
 	// Add a cycle a <-> b <-> c <-> a  (also means 'connect themn all')
-	JoinTowers(a, b)
-	JoinTowers(b, c)
-	JoinTowers(c, a)
+	a.JoinTower(b)
+	b.JoinTower(c)
+	c.JoinTower(a)
 
 	// Verify that each has 1 neighbor
 	for tower := range towers {
-		if num := <-towers[tower].get_num_neighbors; num != 2 {
+		if num := towers[tower].GetNumNeighbors(); num != 2 {
 			t.Error("Tower failed to add a neighbor correctly.")
 		}
 	}
 
 	// Break the chain: a <-> b <-> c
-	DisjoinTowers(c, a)
+	a.DisjoinTower(c)
 
 	// Verify that b has 2 neighbors, but a and c have 1
-	if num := <-a.get_num_neighbors; num != 1 {
+	if num := a.GetNumNeighbors(); num != 1 {
 		t.Error("Tower 'a' didn't disconnect from 'c'")
 	}
-	if num := <-c.get_num_neighbors; num != 1 {
+	if num := c.GetNumNeighbors(); num != 1 {
 		t.Error("Tower 'c' didn't disconnect from 'a'")
 	}
-	if num := <-b.get_num_neighbors; num != 2 {
+	if num := b.GetNumNeighbors(); num != 2 {
 		t.Error("Tower 'b' disconnected but shouldn't have")
+	}
+
+	// Shut down the towers
+	for tower := range towers {
+		towers[tower].Stop()
 	}
 
 }
