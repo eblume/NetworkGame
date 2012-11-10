3e743e57eee7cba364aad4991dfe21a572d2689d
Erich Blume <blume.erich@gmail.com>
2012-11-09 17:12:13 -0800

    Wrap up so I can go home

--- src/main/tower.go
+++ src/main/tower.go
@@ -1,55 +1,20 @@
 package main
 
-const (
-	MAX_HELD_PACKETS  int     = 50
-	PROB_DELETE_STALE float32 = 0.1
-	PROB_PATHFINDER   float32 = 0.1
+import (
+	"sync"
 )
-
-var (
-	// This is *LIKE* a constant, except that during testing we want to set it to
-	// something completely different.
-	SLEEP_PACKET_INTERVAL int = 500
-)
-
-func TOWER_DEBUG() {
-	SLEEP_PACKET_INTERVAL = 1
-}
-
-// For the cache map[*Tower]*towerDistance -> the distance to the key tower,
-// along with the neighboring tower to use to get there.
-type towerDistance struct {
-	via      *Tower
-	distance int
-}
 
 type Tower struct {
-	// Internals
-	neighbors   map[*Tower]bool
-	linktower   chan *Tower
-	unlinktower chan *Tower
-	name        string
-	packets     chan *Packet
-	cache       map[*Tower]*towerDistance
+	name      string
+	neighbors map[*Tower]bool
 }
 
-/////////////// METHODS
-
-// Create a new Tower and start it's handling goroutine
 func NewTower(name string) *Tower {
 	t := &Tower{
-		neighbors:   make(map[*Tower]bool),
-		linktower:   make(chan *Tower),
-		unlinktower: make(chan *Tower),
-		name:        name,
-		packets:     make(chan *Packet, MAX_HELD_PACKETS),
-		cache:       make(map[*Tower]*towerDistance),
+		name:      name,
+		neighbors: make(map[*Tower]bool),
 	}
 	return t
-}
-
-func (t *Tower) Destruct(done chan bool) chan bool {
-	return nil
 }
 
 func (t *Tower) JoinTower(other *Tower, done chan bool) chan bool {
--- src/main/tower_test.go
+++ src/main/tower_test.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"rand"
+	"strconv"
 	"testing"
 )
 
-func TestFake(t *testing.T) {
-	return
+func TestTowerLinking(t *testing.T) {
+	towers := createTowers(10)
+	for i := range towers {
+		if len(towers[i].neighbors) != 0 {
+			t.Error("Tower had neighbor before it should have.")
+		}
+	}
+
+	makeCycle(towers)
+	for i := range towers {
+		if len(towers[i].neighbors) != 2 {
+			t.Error("Tower did not have as many neighbors as it should have.")
+		}
+	}
+}
+
+// Create `count` towers, totally unconnected.
+func createTowers(count int) []*Tower {
+	towers := make([]*Tower, count)
+	for i := range count {
+		towers[i] = NewTower(strconv.Atoi(i))
+	}
+	return towers
+}
+
+// Create a (random) 'tour' of links through the towers.
+// This is not guaranteed to be a NEW cycle, or even to not overlap one.
+func makeCycle(towers []*Tower) {
+	walk := rand.Perm(len(towers))
+	for i := range walk {
+		this := towers[walk[i]]
+		next := towers[walk[(i+1)%len(walk)]]
+		this.JoinTower(next)
+	}
 }
