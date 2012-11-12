d3f116dd0d8bf0f47a1edbb9339b9ada8f1a2681
Erich Blume <blume.erich@gmail.com>
2012-11-11 22:19:28 -0800

    Working join/disjoin

--- src/main/tower.go
+++ src/main/tower.go
@@ -1,26 +1,54 @@
 package main
 
-import (
-	"sync"
-)
+type TowerNeighbors struct {
+	neighbors     map[*Tower]bool
+	join_tower    chan *Tower
+	disjoin_tower chan *Tower
+	stop          chan bool
+}
 
 type Tower struct {
 	name      string
-	neighbors map[*Tower]bool
+	neighbors TowerNeighbors
 }
 
 func NewTower(name string) *Tower {
 	t := &Tower{
-		name:      name,
-		neighbors: make(map[*Tower]bool),
+		name: name,
+		neighbors: TowerNeighbors{
+			neighbors:     make(map[*Tower]bool),
+			join_tower:    make(chan *Tower),
+			disjoin_tower: make(chan *Tower),
+			stop:          make(chan bool, 1),
+		},
 	}
+	go monitor_neighbors(t)
 	return t
 }
 
-func (t *Tower) JoinTower(other *Tower, done chan bool) chan bool {
-	return nil
+func monitor_neighbors(t *Tower) {
+	for {
+		select {
+		case <-t.neighbors.stop:
+			return
+		case other := <-t.neighbors.join_tower:
+			t.neighbors.neighbors[other] = true
+		case other := <-t.neighbors.disjoin_tower:
+			delete(t.neighbors.neighbors, other)
+		}
+	}
 }
 
-func (t *Tower) DisjoinTower(other *Tower, done chan bool) chan bool {
-	return nil
+func (t *Tower) Destroy() {
+	t.neighbors.stop <- true // Nonblocking for the first call
+}
+
+func (t *Tower) JoinTower(other *Tower) {
+	t.neighbors.join_tower <- other
+	other.neighbors.join_tower <- t
+}
+
+func (t *Tower) DisjoinTower(other *Tower) {
+	t.neighbors.disjoin_tower <- other
+	other.neighbors.disjoin_tower <- t
 }
--- src/main/tower_test.go
+++ src/main/tower_test.go
@@ -1,43 +1,58 @@
 package main
 
 import (
-	"rand"
+	"math/rand"
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestTowerLinking(t *testing.T) {
 	towers := createTowers(10)
 	for i := range towers {
-		if len(towers[i].neighbors) != 0 {
+		if len(towers[i].neighbors.neighbors) != 0 {
 			t.Error("Tower had neighbor before it should have.")
 		}
 	}
 
-	makeCycle(towers)
+	makeCycle(towers, t)
 	for i := range towers {
-		if len(towers[i].neighbors) != 2 {
-			t.Error("Tower did not have as many neighbors as it should have.")
+		n := len(towers[i].neighbors.neighbors)
+		if n != 2 {
+			t.Errorf("Tower %v had %v neighbors, expected 2", towers[i].name, n)
 		}
 	}
+
+	destroyTowers(towers)
 }
 
 // Create `count` towers, totally unconnected.
 func createTowers(count int) []*Tower {
 	towers := make([]*Tower, count)
-	for i := range count {
-		towers[i] = NewTower(strconv.Atoi(i))
+	for i := 0; i < count; i++ {
+		towers[i] = NewTower(strconv.Itoa(i))
 	}
 	return towers
 }
 
+func destroyTowers(towers []*Tower) {
+	for i := range towers {
+		towers[i].Destroy()
+	}
+}
+
 // Create a (random) 'tour' of links through the towers.
 // This is not guaranteed to be a NEW cycle, or even to not overlap one.
-func makeCycle(towers []*Tower) {
+func makeCycle(towers []*Tower, t *testing.T) {
 	walk := rand.Perm(len(towers))
 	for i := range walk {
 		this := towers[walk[i]]
 		next := towers[walk[(i+1)%len(walk)]]
-		this.JoinTower(next)
+		// We put this in a goroutine just to stress that the function
+		// is asynchronous - it's asynch even without the go though.
+		go this.JoinTower(next)
 	}
+	// We must sleep because JoinTower is nonblocking and can
+	// become a race condition. Yes this is a kludge.
+	time.Sleep(1 * time.Millisecond)
 }
